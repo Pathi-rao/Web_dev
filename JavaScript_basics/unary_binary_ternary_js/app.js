05a2b3da3e7eaeace4991fa9865b783789991749
Pathi-rao <lakshmipathi0000@gmail.com>
2021-07-15 19:24:29 +0200

    ternary operator

---
+++ JavaScript_basics/unary_binary_ternary_js/app.js
@@ -0,0 +1,10 @@
+// Ternary operator
+
+const value = 1 < 2
+
+value ? console.log('Value is True') : console.log('Value is False')
+
+// compact way of writing if else conditions
+
+/* Fun fact: If you have loom extension enabled on chrome, it throws an error sometimes. 
+We can fix it either updating or removing that extension */
