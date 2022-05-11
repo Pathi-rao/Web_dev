ea5e16528b12823f68ac05171622540921bd5bfc
Mandeep Sai Bandi <mandeep.bandi@nexocraft.com>
2022-05-11 08:41:12 +0200

    improve sidebar

--- webapp/src/components/Sidebar/Sidebar.jsx
+++ webapp/src/components/Sidebar/Sidebar.jsx
@@ -1,34 +1,43 @@
 import React from "react"
 import './Sidebar.css'
 import {Link} from "react-router-dom"
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import {faHome ,faVideo,faFile,faCodeBranch,faImages } from '@fortawesome/fontawesome-free-solid'
+
+
 
 class Sidebar extends React.Component{
     render(){
         return (
             <div className="sidebar">
 
-                <div>
-                <Link to={"/"}>
-                    Home
-                </Link>
+                <div className="sidebar_items">
+                    <FontAwesomeIcon icon={faHome} />
+                    <Link to={"/"}>
+                        Home
+                    </Link>
                 </div>
 
-                <div>
+                <div className="sidebar_items">
+                <FontAwesomeIcon icon={faVideo} />
                     <Link to={"/about"}>
                     Video player
                     </Link>
                 </div>
 
-                <div>
-                    Masking
+                <div className="sidebar_items">
+                <FontAwesomeIcon icon={faImages} />
+                   <a href=""> Masking</a> 
                 </div>
 
-                <div>
-                    Execution
+                <div className="sidebar_items">
+                <FontAwesomeIcon icon={faCodeBranch} />
+                   <a href=""> Execution</a> 
                 </div>
 
-                <div>
-                    Result
+                <div className="sidebar_items">
+                <FontAwesomeIcon icon={faFile} />
+                  <a href=""> Result</a>  
                 </div>
 
             </div>
